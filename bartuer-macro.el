a4c52a03e5c188ee3bd72aafe28437ec47f1ae55
bartuer <bartuer@bartuer-vm.(none)>
2008-09-06 23:40:33 -0400

    add macro

---
+++ bartuer-macro.el
@@ -0,0 +1,6 @@
+
+(fset '2-floor
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("afloorlfloor>\\\\>l" 0 #("%d" 0 2 (auto-composed t)))) arg)))
+
+(fset 'mark-hide
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("	" 11 " [%d]")) arg)))
