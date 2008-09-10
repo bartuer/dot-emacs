f22118daef770edd91b2f5022d84711d82c0c063
CT413 Zhou HaiTao-KWG874 <kwg874@zch68ha01.ap.mot.com>
2008-09-10 12:20:13 +0800

    ;; clean clearcase binding, now only add diff utils.  Design new ;; feature of clearcase mode, document and code for implement is ;; ready.

--- bartuer-c.el
+++ bartuer-c.el
@@ -5,5 +5,6 @@
   ;; is it possible to guess the code style ?
   ;; now I using the c-file-style in file varible
   (define-key c-mode-base-map "\C-m" 'c-context-line-break)
+  (define-key makefile-mode-map "\C-j" 'recompile)
   (define-key c-mode-base-map "\C-j" 'recompile))
 
