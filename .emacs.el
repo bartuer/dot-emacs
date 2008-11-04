048e938bd9c470edf1df787fdbc6fe1445371e0f
bartuer <bartuer@bartuer-desktop.(none)>
2008-11-05 06:21:18 +0800

    start-scheme is more flexible way to begin scheme evaluate in emacs. just remember the command name, do not need bind it to a key.

--- .emacs.el
+++ .emacs.el
@@ -346,6 +346,7 @@ If give a negative ARG, will undo the last mark action, thus the
 (autoload 'bartuer-txt-load "bartuer-txt.el" "for text mode" t)
 (add-hook 'text-mode-hook 'bartuer-txt-load)
 
+(require 'xscheme nil t)
 (autoload 'bartuer-scheme-load "bartuer-scheme.el" "for scheme mode" t)
 (add-hook 'scheme-mode-hook 'bartuer-scheme-load)
 (add-to-list 'auto-mode-alist '("\\.scm$" . scheme-mode))
