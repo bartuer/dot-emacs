7e2a6b8b210edea09dac6a36165601d9e8f75295
bartuer <bartuer@bartuer-vm.(none)>
2008-11-04 16:59:41 -0500

    add C-j binding in scheme mode

--- bartuer-scheme.el
+++ bartuer-scheme.el
@@ -2,4 +2,7 @@
   "for scheme language
 "
   (load "xscheme.el")
-  )
+  (when (fboundp 'advertised-xscheme-send-previous-expression)
+    (define-key scheme-mode-map "\C-j" 'advertised-xscheme-send-previous-expression)
+    (define-key scheme-interaction-mode-map "\C-j" 'advertised-xscheme-send-previous-expression)
+  ))
