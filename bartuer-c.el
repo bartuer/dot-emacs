86e2fb26c313fdad7f7033008d00b4d81a08290d
bartuer <bartuer@bartuer-desktop.(none)>
2008-11-14 03:15:39 +0800

    c mode dabbrev-expand binding

--- bartuer-c.el
+++ bartuer-c.el
@@ -5,6 +5,7 @@
   (c-subword-mode 1)
   ;; is it possible to guess the code style ?
   ;; now I using the c-file-style in file varible
+  (define-key c-mode-base-map "\M-j" 'dabbrev-expand)
   (define-key c-mode-base-map "\C-m" 'c-context-line-break)
   (define-key makefile-mode-map "\C-j" 'recompile)
   (define-key c-mode-base-map "\C-j" 'recompile))
