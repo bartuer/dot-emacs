a79c4210e8db03955c9c7710a4815159bc4c6d07
bartuer <bartuer@bartuer.local>
2009-02-22 17:44:52 +0800

    fix the wrong key bind

--- bartuer-org.el
+++ bartuer-org.el
@@ -5,8 +5,8 @@
   (global-set-key "\C-cb" 'org-iswitchb)
   (turn-on-font-lock)
   (define-key org-mode-map "\M-0" 'org-occur)
-  (define-key org-mode-map "S-TAB" 'org-shifttab)
   (define-key org-mode-map "\C-j" 'org-meta-return)
+  (define-key org-mode-map "<backtab>" 'org-shifttab)
   ;; add a hook when saving also export to a html
   ;; setup a webserver, can quick access one page org content locally
 )
