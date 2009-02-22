a7cddbf39153ad538e3b6741490bcb50ca133db1
bartuer <bartuer@bartuer.local>
2009-02-22 10:41:11 +0800

    add statistic and org mode file

---
+++ bartuer-org.el
@@ -0,0 +1,13 @@
+(defun bartuer-org-load ()
+  "for org mode"
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
+  (turn-on-font-lock)
+  (define-key org-mode-map "\M-0" 'org-occur)
+  (define-key org-mode-map "S-TAB" 'org-shifttab)
+  (define-key org-mode-map "\C-j" 'org-meta-return)
+  ;; add a hook when saving also export to a html
+  ;; setup a webserver, can quick access one page org content locally
+  
+)
