6249efc65bc8e4788baa4da53dd178af583baca5
bartuer <bartuer@gmail.com>
2009-03-13 06:22:26 +0800

    add yaml

--- bartuer-rails.el
+++
@@ -1,9 +0,0 @@
-(defun bartuer-rails-load
-  "mark language mode "
-  (require 'rinari nil t)
-  (require 'gist nil t)
-  (autoload 'css-mode "css-mode-simple.el"
-             "mode for css file" t nil)
-  (require 'css-mode nil t)
-  (require 'rhtml-mode nil t)
-  )
