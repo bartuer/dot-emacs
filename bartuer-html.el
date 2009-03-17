f017801cfb5def80187cea0271eb78e156294aa0
bartuer <bartuer@gmail.com>
2009-03-17 11:43:30 +0800

    fix typo

--- bartuer-html.el
+++ bartuer-html.el
@@ -1,7 +1,7 @@
 (defun bartuer-html-load ()
   "html mode modification"
   (define-key sgml-mode-map "\C-\M-f" 'sgml-skip-tag-forward)
-  (define-key sgml-mode-map "\C-\M-b" 'sgmll-skip-tag-backward)
-  (define-key sgml-mode-map "\C-\M-k" 'sgmll-delete-tag)
+  (define-key sgml-mode-map "\C-\M-b" 'sgml-skip-tag-backward)
+  (define-key sgml-mode-map "\C-\M-k" 'sgml-delete-tag)
   (define-key sgml-mode-map "\C-\M-v" 'sgml-validate)
-  (define-key sgml-mode-map "\C-c\C-ct" 'sgmll-close-tag))
+  (define-key sgml-mode-map "\C-c\C-ct" 'sgml-close-tag))
