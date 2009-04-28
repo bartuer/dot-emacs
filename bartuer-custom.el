5f89a6a8d9f3dfe80a9b6ebcc727030212dbf26b
bartuer <bartuer@gmail.com>
2009-04-29 06:53:54 +0800

    include framework
    [[info:gcc-4.1.info.gz:Darwin%20Options][how to include frameworks]]

--- bartuer-custom.el
+++ bartuer-custom.el
@@ -33,6 +33,7 @@
  '(find-grep-options "-vE \".svn|drw|svn-base|*.pyc\"")
  '(find-ls-option (quote ("-exec ls -ld {} \\;" . "-ld")))
  '(flymake-allowed-file-name-masks (quote (("\\.c\\'" flymake-simple-make-init) ("\\.m\\'" flymake-objc-init) ("\\.cpp\\'" flymake-simple-make-init) ("\\.xml\\'" flymake-xml-init) ("\\.html?\\'" flymake-xml-init) ("\\.cs\\'" flymake-simple-make-init) ("\\.p[ml]\\'" flymake-perl-init) ("\\.php[345]?\\'" flymake-php-init) ("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup) ("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup) ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup) ("\\.tex\\'" flymake-simple-tex-init) ("\\.idl\\'" flymake-simple-make-init))))
+ '(flymake-objc-compile-options (quote ("-I." "-F../Frameworks")))
  '(flyspell-use-meta-tab nil)
  '(gdb-debug-log-max nil)
  '(gdb-enable-debug t)
