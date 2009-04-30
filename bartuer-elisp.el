ea5bbce25c3ce4963dbd6ff7e0298a76bde15111
bartuer <bartuer@gmail.com>
2009-05-01 06:19:58 +0800

    elisp debug shortcut

--- bartuer-elisp.el
+++ bartuer-elisp.el
@@ -1,3 +1,9 @@
 (defun bartuer-elisp-load ()
   "added when edit elisp file"
+  (define-key emacs-lisp-mode-map "\C-c\C-i" (lambda ()
+                                               (interactive)
+                                               (insert "(interactive)")))
+  (define-key emacs-lisp-mode-map "\C-c\C-c" (lambda ()
+                                               (interactive)
+                                               (insert "(debug)")))
   (define-key emacs-lisp-mode-map "\M-i" 'PC-lisp-complete-symbol))
