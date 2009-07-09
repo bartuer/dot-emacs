7f5cda9bc2cef14979050f33eca6b6b62e1af827
bartuer <bartuer@gmail.com>
2009-07-09 12:52:15 +0800

    filter out <definition-*> in js etags completion

--- anything-yasnippet.el
+++ anything-yasnippet.el
@@ -133,7 +133,7 @@
          (lambda ()
            (condition-case x
                (setq anything-yasnippet-completion-table (mapcar 'anything-js-etags-parser
-                                                             (split-string (shell-command-to-string "cat ~/etc/el/js/TAGS|grep ^[a-zA-Z0-9_].*") "\n")))
+                                                             (split-string (shell-command-to-string "cat ~/etc/el/js/TAGS|grep ^[a-zA-Z0-9_].* |sed 's+\.<definition-[0-9]*>++g'|uniq") "\n")))
              (error (setq anything-yasnippet-completion-table nil))
              )
            )
