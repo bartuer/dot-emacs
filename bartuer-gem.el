3561991a6c0c49afd43104319de730f5ebe0859d
bartuer <bartuer@gmail.com>
2009-07-20 04:47:49 +0800

    bartuer-mongrel

--- bartuer-gem.el
+++ bartuer-gem.el
@@ -49,4 +49,53 @@ candidate help, get result from ruby compilation mode"
             (with-output-to-temp-buffer (format "gem help %s" item)
               (princ (shell-command-to-string (concat "gem help " item))))))))
 
+(defun pcmpl-mongrel-commands ()
+  "Return a list of all the mongrel commands"
+  (delq nil
+        (mapcar '(lambda(line)
+                   (if (string-match "^ - \\([a-z:]+\\).*$" line) (match-string 1 line)))
+                (split-string (shell-command-to-string "mongrel_rails") "[\n]"))))
+
+(defun ruby-compilation-mongrel (&optional edit task env-vars)
+  "Run a mongrel process dumping output to a ruby compilation buffer."
+  (interactive "P")
+  (let* ((task (concat
+		(or task (if (stringp edit) edit)
+		    (completing-read "Mongrel: " (pcmpl-mongrel-commands)))
+		" "
+		(mapconcat (lambda (pair)
+			     (format "%s=%s" (car pair) (cdr pair)))
+			   env-vars " ")))
+	 (mongrel-args
+          ((lambda (command)
+             (pop-to-buffer
+              (concat "mongrel help "
+                      ((lambda  (s)
+                         "Remove whitespace at beginning and end of string."
+                         (if (string-match "\\`[ \t\n\r]+" s) (setq s (replace-match "" t t s)))
+                         (if (string-match "[ \t\n\r]+\\'" s) (setq s (replace-match "" t t s)))
+                         s) command)))
+             (read-from-minibuffer "Edit Mongrel Command: " (concat task " "))) task)))
+    (pop-to-buffer (ruby-compilation-do
+                    "mongrel" (cons "mongrel_rails"
+                                (split-string mongrel-args))))))
+
+
+(defun bartuer-mongrel (&optional task edit-cmd-args)
+  "issue mongrel command, see help from completion and icicle's
+candidate help, get result from ruby compilation mode"
+  (interactive "P")
+  (ruby-compilation-mongrel task edit-cmd-args
+			(if rinari-rails-env (list (cons "RAILS_ENV" rinari-rails-env)))))
+
+
+(defadvice bartuer-mongrel (before icicle-mongrel-help activate)
+  "do right thing for icicle-candidate-help-fn ."
+  (setq icicle-candidate-help-fn
+        (lambda  (entry)
+          "return the mongrel commands documents of entry"
+          (let ((item (widget-princ-to-string entry)))
+            (with-output-to-temp-buffer (format "mongrel help %s" item)
+              (princ (shell-command-to-string (concat "mongrel_rails " item " -h"))))))))
+
 (provide 'bartuer-gem)
