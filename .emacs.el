2a7f94c7fa6153b8f96acc0b67f9451287a73b2d
bartuer <bartuer@gmail.com>
2009-07-25 09:52:41 +0800

    advice name could not be a function

--- .emacs.el
+++ .emacs.el
@@ -546,11 +546,12 @@ If give a negative ARG, will undo the last mark action, thus the
                                         ; need check fastri-server and
                                         ; start it?
 
-(defun icicle-ri-help ()
+(defadvice ri-ruby-read-keyw (before icicle-ri-help activate)
   "do right thing for icicle-candidate-help-fn ."
-  (setq icicle-candidate-help-fn 'bartuer-ruby-ri))
-(defadvice ri-ruby-read-keyw (before icicle-ri-help activate))
-(defadvice inf-ruby-completions (before icicle-ri-help activate))
+  (setq icicle-candidate-help-fn 'bartuer-ruby-ri)))
+(defadvice inf-ruby-completions (before icicle-ri-help activate)
+    "do right thing for icicle-candidate-help-fn ."
+  (setq icicle-candidate-help-fn 'bartuer-ruby-ri)) 
 
 
 (autoload 'css-mode "css-mode-simple.el"
