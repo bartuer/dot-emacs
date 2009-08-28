19b4980aba02a4cff63c07fd8a1f45afa9e87518
bartuer <bartuer@gmail.com>
2009-08-28 12:40:00 +0800

    migration rinari to javascript project

--- bartuer-js.el
+++ bartuer-js.el
@@ -1,9 +1,6 @@
 (require 'moz nil t)
 (require 'bartuer-js-inf nil t)
 
-(defun js-test-toggle ()
-  )
-
 (defun autotest ()
   (interactive)
   (cd "..")
@@ -55,6 +52,137 @@ it is suitable to browse OO hierarchy"
                                                                      ))))
   (anything-etags-select))
 
+(defun js-project-root ()
+  "find current root for javascript project"
+  (cond ((eq 0 (string-match
+                (expand-file-name "~/local/src/prototype")
+                (expand-file-name (buffer-file-name))))
+         (expand-file-name "~/local/src/prototype/"))
+        ((or
+          (eq 0 (string-match
+                 "\\(.*\\)/spec/"
+                 (expand-file-name (buffer-file-name))))
+          (eq 0 (string-match
+                 "\\(.*\\)/lib/"
+                 (expand-file-name (buffer-file-name))))
+          (eq 0 (string-match
+                 "\\(.*\\)/spec/fixtures/"
+                 (expand-file-name (buffer-file-name)))))
+         (concat (match-string 1
+                       (expand-file-name (buffer-file-name)))
+                 "/"))
+        (t
+          (expand-file-name (file-name-directory (buffer-file-name))))))
+
+(setf
+ js-jump-schema
+ '(
+   (prototype-testimp
+    (("test/unit/\\1_test.js"                   . "src/lang/\\1.js")
+     ("test/unit/\\1_test.js"                   . "src/dom/\\1.js")
+     ("test/unit/base_test.js"                  . "src/prototype.js")
+     ("test/unit/prototype_test.js"             . "src/prototype.js")
+     ("test/unit/position_test.js"              . "src/deprecated.js")
+     (t                                         . "src/.*"))
+    nil)
+   (prototype-test
+    (("src/lang/\\1.js"                         . "test/unit/\\1_test.js")
+     ("src/dom/\\1.js"                          . "test/unit/\\1_test.js")
+     ("src/ajax/\\1.js"                         . "test/unit/ajax_test.js")
+     (t                                         . "test/unit/.*"))
+    nil)
+   (prototype-fixture
+    (("test/unit/hash_test.js"                  . "test/unit/fixtures/hash.js")
+     (t                                         . "test/unit/fixtures/.*"))
+    nil)
+   (jspecimp
+    (("spec/spec.\\1.js"                        . "lib/\\1.js")
+     ("spec/fixtures/\\1.js"                    . "lib/\\1.js")
+     (t                                         . "lib/.*js"))
+     nil)
+   (jspec
+    (("lib/\\1.js"                              . "spec/spec.\\1.js")
+     ("spec/fixtures/\\1.js"                    . "spec/spec.\\1.js")
+     (t                                         . "spec/spec.*"))
+    t);here need a lambda generate a new test file
+   (jspec-fixture
+    (("spec/spec.\\1.js"                        . "spec/fixtures/\\1.js")
+     ("lib/\\1.js"                              . "spec/fixtures/\\1.js")
+     (t                                         . "spec/fixtures/.*"))
+    nil)
+   (file-in-project ((t . ".*")) nil)
+   ))
+
+(defun js-apply-jump-schema (schema)
+  "This function takes a of SCHEMA s.t. each element in the list
+can be fed to `defjump'.  This is used to define all of the
+rinari-find-* functions, and can be used to customize their
+behavior."
+  (mapcar
+   (lambda (type)
+     (let ((name (first type))
+	   (specs (second type))
+	   (make (third type)))
+       (eval `(defjump
+		(quote ,(read (format "js-find-%S" name)))
+		(quote ,specs)
+		'js-project-root
+		,(format "Go to the most logical %S given the current location" name)
+		,(if make `(quote ,make))
+		))))
+   schema))
+
+(js-apply-jump-schema js-jump-schema)
+
+(defun js-smart-toggle ()
+  "depend on current buffer, switch to the buffer most possible, DWIM"
+  (interactive)
+  (cond ((eq 0 (string-match
+                 "\\(.*\\)/spec/"
+                 (expand-file-name (buffer-file-name))))
+         (js-find-jspecimp))
+        ((eq 0 (string-match
+                 "\\(.*\\)/lib/"
+                 (expand-file-name (buffer-file-name)))
+             )
+         (js-find-jspec)
+         )
+        ((eq 0 (string-match
+                 "\\(.*\\)/spec/fixtures"
+                 (expand-file-name (buffer-file-name)))
+             )
+         (js-find-jspec)
+         )
+        ((eq 0 (string-match
+                 (expand-file-name "~/local/src/prototype/test/unit/fixtures")
+                 (expand-file-name (buffer-file-name)))
+             )
+         (js-find-prototype-test)
+         )
+        ((eq 0 (string-match
+                 (expand-file-name "~/local/src/prototype/test/unit")
+                 (expand-file-name (buffer-file-name)))
+             )
+         (js-find-prototype-testimp)
+         )
+        ((eq 0 (string-match
+                 (expand-file-name "~/local/src/prototype/src")
+                 (expand-file-name (buffer-file-name)))
+             )
+         (js-find-prototype-test)
+         ))
+  )
+  
+(defun js-toggle ()
+  "js version `rinari-ido'"
+  (interactive)
+  (let* ((js-toggle-target (ido-completing-read "Jump to :" 
+                                                 (list  "file-in-project"
+                                                        "jspec" "jspec-fixture" "jsepcimp"
+                                                        "prototype-fixture" "prototype-test" "prototype-testimp") nil t)))
+    (apply (intern (concat "js-find-" js-toggle-target)) nil))
+  )
+
 (defun bartuer-js-load ()
   "for javascript language
 "
@@ -67,8 +195,8 @@ it is suitable to browse OO hierarchy"
   (flymake-mode t)
   (setq js2-mode-show-overlay t)
   (defalias  'w 'js2-mode-show-node)
-  (define-key js2-mode-map "\C-cj" 'js-test-toggle)
-  (define-key js2-mode-map "\C-c\C-j" 'js-test-toggle)
+  (define-key js2-mode-map "\C-cj" 'js-smart-toggle)
+  (define-key js2-mode-map "\C-c\C-j" 'js-toggle)
   (define-key js2-mode-map "\C-\M-n" 'js2-next-error)
   (define-key js2-mode-map "\C-c\C-u" 'js2-show-element)
   (define-key js2-mode-map "\C-c\C-s" 'connect-jsh)
