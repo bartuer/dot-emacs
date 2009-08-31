cf86008a7068c8dba7e86885db726d1ade500f70
bartuer <bartuer@gmail.com>
2009-09-01 07:22:38 +0800

    jscore object

---
+++ js/jscore/obj.js
@@ -0,0 +1,31 @@
+var obj = {};
+
+/**
+ * The hasOwnProperty method returns true if this conrains a
+ * property.  The prototype chain is notexamined.  THis method is
+ * useless if the name is hasOwnProperty.
+ *
+ * @param name
+ * @return boolean
+ */
+obj.hasOwnProperty = function (name) {
+
+};
+
+var o = {};
+o.hasOwnProperty('length');         //#=> false
+o.hasOwnProperty('hasOwnProperty'); //#=> false
+Object.prototype.hasOwnProperty('hasOwnProperty'); //#=> true
+
+if (typeof Object.create !== 'function') {
+  Object.create = function (o) {
+    var F = function () {};
+    F.prototype = o;
+    return new F();
+  };
+}
+
+o.name = 'proptotype chain';
+var b = Object.create(o);
+b.hasOwnProperty('name');       //#=> false
+b.name;                         //#=> 'proptotype chain'
