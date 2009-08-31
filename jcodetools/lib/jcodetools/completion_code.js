340bc7eeec6a5265a03f43cec0840506c94fe55b
bartuer <bartuer@gmail.com>
2009-09-01 07:22:01 +0800

    add debug code for fix breaked jxmp test

--- jcodetools/lib/jcodetools/completion_code.js
+++ jcodetools/lib/jcodetools/completion_code.js
@@ -42,3 +42,105 @@ var test_obj = {
 
 
 
+
+load('/Users/bartuer/etc/el/js/env.js');
+var nav = window.navigator;
+load('/Users/bartuer/local/src/jspec/lib/jspec.js');
+function inspector(jct_target) {
+  var members = [];
+  var jct_k;
+  for (jct_k in jct_target.prototype) {
+    if (jct_target[jct_k] === null) {
+      members.push('NULL ' + jct_k + + '|' + jct_k);
+    }
+    else if (jct_target[jct_k].constructor === Array) {
+      members.push('ARRAY ' + jct_k + '[]' + '|' + jct_k);
+    } else if (jct_target[jct_k].toString() === '[object Object]') {
+      members.push('OBJ ' + jct_k + '{}' + '|' + jct_k);
+    } else if (typeof jct_target[jct_k] === 'function')
+      members.push('FUNC ' + jct_k + 'function(){}' + '|' + jct_k);
+    else {
+      members.push('ELSE ' + jct_k + '|' + jct_k);
+    }
+  }
+  return members;
+}
+
+function pinspector(jct_target) {
+  var members = [];
+  var jct_k;
+
+  if (typeof jct_target) {
+    for (jct_k in String.prototype) {
+      if (jct_target[jct_k] === null) {
+        members.push('NULL ' + jct_k  + '|' + jct_k);
+      }
+      else if (jct_target[jct_k].constructor === Array) {
+        members.push('ARRAY ' + jct_k + '[]' + '|' + jct_k);
+      } else if (jct_target[jct_k].toString() === '[object Object]') {
+        members.push('OBJ ' + jct_k + '{}' + '|' + jct_k);
+      } else if (typeof jct_target[jct_k] === 'function') {
+        members.push('FUNC ' + jct_k + 'function(){}' + '|' + jct_k);
+      } else {
+        members.push('ELSE ' + jct_k + '|' + jct_k);
+      }
+    }
+
+    return members;
+    }
+}
+
+i = [2, 4]                      //#=> [2, 4]
+
+inspector(nav)                  //
+
+inspector(i)                    //
+
+function defun(name,  from, to) {
+    this.headers = {};
+    this.responseHeaders = {};
+}
+
+
+function cook(chicken,pork,beef) {
+
+}
+
+var test_obj = {
+  a: [1,2,3,4,5,6,7] ,
+  d: {one:1,
+  two: '2',
+  three: true
+  },
+  bbbbb:false,
+  p: defun,
+  place: cook
+};
+
+test_obj.dom = function(name, num) {
+
+};
+
+typeof test_obj.d.one                  //#=> 'number'
+
+String.prototype
+typeof String.prototype
+String.prototype.length
+String.length
+typeof String
+
+str = new String();
+pinspector(str)                 //#=> []
+
+for (jjjc in test_obj) {
+  jjjc                          //#=> 'a', 'd', 'bbbbb', 'p', 'place', 'dom'
+}
+
+if (typeof Object.create !== 'function') {
+  Object.create = function (o) {
+    var F = function () {};
+    F.prototype = o;
+    return new F();
+  };
+}
+
