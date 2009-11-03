afc7488d84e245bc50c7135d3def430152ee38c5
Cloudera Training <training@localhost.(none)>
2009-11-03 03:34:02 -0800

    c improvement alpha

--- bartuer-c.el
+++ bartuer-c.el
@@ -1,3 +1,10 @@
+(defun bartuer-c-indent ()
+  (interactive)
+  (if (not (window-minibuffer-p))
+      (cond ((indent-for-tab-command))
+            (t (indent-relative-maybe))
+            (t (indent-for-comment)))))
+
 (defun bartuer-c-common ()
   "for c and C++ language
 "
@@ -5,6 +12,7 @@
   (c-subword-mode 1)
   ;; is it possible to guess the code style ?
   ;; now I using the c-file-style in file varible
+  (define-key c-mode-base-map "\C-i" 'bartuer-c-indent)
   (define-key c-mode-base-map "\M-j" 'dabbrev-expand)
   (define-key c-mode-base-map "\C-m" 'c-context-line-break)
   (define-key makefile-mode-map "\C-j" 'recompile)
