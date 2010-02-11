da2cbd6520983686162da08594507144c62a318c
bartuer <bartuer@gmail.com>
2010-02-11 11:57:46 +0800

    tidy flymake

--- flymake.el
+++ flymake.el
@@ -1805,6 +1805,22 @@ Use CREATE-TEMP-F for creating temp copy."
 (defun flymake-xml-init ()
   (list "xmllint" (list (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))))
 
+;;;; for html, use tidy 
+(defun flymake-html-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "tidy" (list local-file))))
+
+(add-to-list 'flymake-allowed-file-name-masks
+             '("\\.html?$" flymake-html-init))
+
+(add-to-list 'flymake-err-line-patterns
+             '("line \\([0-9]+\\) column \\([0-9]+\\) - \\(Warning\\|Error\\): \\(.*\\)"
+               nil 1 2 4))
+
 (provide 'flymake)
 
 ;; arch-tag: 8f0d6090-061d-4cac-8862-7c151c4a02dd
