284a9abb1f356571c88359ba6fe4d53451ce9763
bartuer <bartuer@gmail.com>
2010-08-17 16:35:22 +0800

    org capture setting

--- bartuer-org.el
+++ bartuer-org.el
@@ -11,6 +11,46 @@
     '("0:05" "0:10" "0:15" "0:20" "0:30" "0:45" "1:00" "1:30" "2:00" "3:00" "4:00" "5:00" "6:00" "8:00"))
    (t nil)))
 
+
+(defun bartuer-capture-insert-link ()
+  (interactive)
+  (with-current-buffer (get-buffer-create "*org-link-temp*")
+    (org-insert-link)
+    (setq org-currnet-link-string (buffer-substring (point-min) (point-max)))
+    (kill-region (point-min) (point-max)))
+  org-currnet-link-string
+)
+
+(defun bartuer-setup-capture ()
+  "capture templates"
+  (interactive)
+  (setq org-capture-templates
+      '(("d" "Debug" entry (file+headline "~/org/next.org" "Debuging")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %a\n  %i\n")
+        ("f" "Fix" entry (file+headline "~/org/next.org" "Fixing")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %i\n")
+        ("$" "Buy" entry (file+headline "~/org/next.org" "Buying")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %i\n")
+        ("b" "Build" entry (file+headline "~/org/next.org" "Building")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %i\n")
+        ("h" "Hack" entry (file+headline "~/org/next.org" "Hacking")
+             "* TODO %?\n %a %^{SCHEDULED}p\n  %i\n")
+        ("m" "Meet" entry (file+headline "~/org/next.org" "Meeting")
+             "* TODO %?\n %a %^{SCHEDULED}p\n  %i\n")
+        ("s" "Research" entry (file+headline "~/org/next.org" "Researching")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %(bartuer-capture-insert-link)\n  %i\n")
+        ("r" "Read" entry (file+headline "~/org/next.org" "Reading")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %(bartuer-capture-insert-link)\n  %i\n")
+        ("w" "Watch" entry (file+headline "~/org/next.org" "Watching")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %(bartuer-capture-insert-link)\n  %i\n")
+        ("t" "Todo" entry (file+headline "~/org/next.org" "!Category")
+             "* TODO %?\n  %^{SCHEDULED}p\n  %i\n")
+        ("c" "Capture" plain (file "~/org/quote.org")
+             "* %?\n%U\n%c\n%i\n")
+        ("j" "Journey" plain (file+datetree "~/org/diary.org")
+             "      - %?\n       %U\n  %i\n")
+        )))
+
 (defun bartuer-org-load ()
   "for org mode"
   (global-set-key "\C-cl" 'org-store-link)
