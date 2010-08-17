5f064e29ceb89222dac9ee29b76d7671513065f6
bartuer <bartuer@gmail.com>
2010-08-17 19:25:19 +0800

    let ido select time

--- org/org.el
+++ org/org.el
@@ -14162,6 +14162,30 @@ So these are more for recording a certain time/date."
 (defvar org-read-date-history nil)
 (defvar org-read-date-final-answer nil)
 
+(setq possible-time-in-day (list "00:00" "00:15" "00:30" "00:45"
+                                 "01:00" "01:15" "01:30" "01:45"
+                                 "02:00" "02:15" "02:30" "02:45"
+                                 "03:00" "03:15" "03:30" "03:45"
+                                 "04:00" "04:15" "04:30" "04:45"
+                                 "05:00" "05:15" "05:30" "05:45"
+                                 "06:00" "06:15" "06:30" "06:45"
+                                 "07:00" "07:15" "07:30" "07:45"
+                                 "08:00" "08:15" "08:30" "08:45"
+                                 "09:00" "09:15" "09:30" "09:45"
+                                 "10:00" "10:15" "10:30" "10:45"
+                                 "11:00" "11:15" "11:30" "11:45"
+                                 "12:00" "12:15" "12:30" "12:45"
+                                 "13:00" "13:15" "13:30" "13:45"
+                                 "14:00" "14:15" "14:30" "14:45"
+                                 "15:00" "15:15" "15:30" "15:45"
+                                 "16:00" "16:15" "16:30" "16:45"
+                                 "17:00" "17:15" "17:30" "17:45"
+                                 "18:00" "18:15" "18:30" "18:45"
+                                 "19:00" "19:15" "19:30" "19:45"
+                                 "20:00" "20:15" "20:30" "20:45"
+                                 "21:00" "21:15" "21:30" "21:45"
+                                 "22:00" "22:15" "22:30" "22:45"
+                                 "23:00" "23:15" "23:30" "23:45"))
 (defun org-read-date (&optional with-time to-time from-string prompt
 				default-time default-input)
   "Read a date, possibly a time, and make things smooth for the user.
@@ -14237,7 +14261,7 @@ user."
 	 (timestr (format-time-string
 		   (if with-time "%Y-%m-%d %H:%M" "%Y-%m-%d") def))
 	 (prompt (concat (if prompt (concat prompt " ") "")
-			 (format "Date+time [%s]: " timestr)))
+			 (format "Date [%s]: " timestr)))
 	 ans (org-ans0 "") org-ans1 org-ans2 final)
 
     (cond
@@ -14311,8 +14335,9 @@ user."
 		(progn
 		  (use-local-map map)
 		  (add-hook 'post-command-hook 'org-read-date-display)
-		  (setq org-ans0 (read-string prompt default-input
-					      'org-read-date-history nil))
+		  (setq org-ans0 (concat (read-string prompt default-input nil nil) " "
+                                         (ido-completing-read "time: " possible-time-in-day)
+                        ))
 		  ;; org-ans0: from prompt
 		  ;; org-ans1: from mouse click
 		  ;; org-ans2: from calendar motion
@@ -14321,7 +14346,8 @@ user."
 	      (use-local-map old-map)
 	      (when org-read-date-overlay
 		(delete-overlay org-read-date-overlay)
-		(setq org-read-date-overlay nil)))))))
+		(setq org-read-date-overlay nil))))
+          )))
 
      (t ; Naked prompt only
       (unwind-protect
