2c5ccc7d186aef1798bebf51d4e3804b855ecc03
bartuer <bartuer@gmail.com>
2010-08-18 11:32:26 +0800

    jump from org to archive

--- bartuer-org.el
+++ bartuer-org.el
@@ -51,8 +51,13 @@
              "      - %?\n       %U\n  %i\n")
         )))
 
+(defun bartuer-jump-to-archive ()
+  (interactive)
+  (find-file (org-extract-archive-file)))
+
 (defun bartuer-org-load ()
   "for org mode"
+  (defalias 'ar 'bartuer-jump-to-archive)
   (global-set-key "\C-cl" 'org-store-link)
   (global-set-key "\C-ca" 'org-agenda)
   (global-set-key "\C-cb" 'org-sparse-tree)
