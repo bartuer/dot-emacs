1313cce016bd7966083e791c7310dfd4dcf1307d
bartuer <bartuer@gmail.com>
2010-12-03 10:42:44 +0800

    fix sql -> orgtbl space bug

--- bartuer-sql.el
+++ bartuer-sql.el
@@ -2,10 +2,10 @@
   (interactive)
   (with-current-buffer (get-buffer-create "*sql2orgtbl*")
     (kill-region (point-min) (point-max)))
-  (shell-command-on-region (point-min) (point-max) "sqlite3 -column -batch"
+  (shell-command-on-region (point-min) (point-max) "sqlite3 -csv -batch"
                            "*sql2orgtbl*" nil (get-buffer-create "*sql2orgtbl-error"))
   (with-current-buffer "*sql2orgtbl*"
-    (org-table-convert-region (point-min) (point-max))
+    (org-table-convert-region (point-min) (point-max) '(4))
     (org-mode)
     (goto-char (point-min)))
   (pop-to-buffer "*sql2orgtbl*")
