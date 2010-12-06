06f1ab2f91cdf715afda148a1fabb33437a36192
bartuer <bartuer@gmail.com>
2010-12-07 07:08:50 +0800

    sql-send-buffer-dwim

--- bartuer-sql.el
+++ bartuer-sql.el
@@ -15,11 +15,21 @@
   (interactive)
   (sql-sqlite)
   (sql-set-sqli-buffer-generally)
-)
+  )
+
+(defun sql-send-buffer-dwim ()
+  (interactive)
+  (if (buffer-live-p sql-buffer)
+      (sql-send-buffer)
+    (shell-command-on-region (point-min) (point-max)
+                             (concat "sqlite3 "
+                                     (replace-regexp-in-string "sql" "db" (buffer-file-name)))))
+  )
 
 (defun bartuer-sql-load ()
   "for sql mode"
   (define-key sql-mode-map "\C-c\C-s" 'connect-sqlite-i-buffer)
   (define-key sql-mode-map "\C-c\C-e" 'sql-send-string)
   (define-key sql-mode-map "\C-c\C-t" 'sql-to-orgtbl)
-  )
+  (define-key sql-mode-map "\C-c\C-b" 'sql-send-buffer-dwim))
+)
