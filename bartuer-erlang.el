47d25c8b79bbc26d26bc60bea45a79aad111044c
bartuer <bartuer@gmail.com>
2011-01-10 22:06:39 +0800

    use empty skel

--- bartuer-erlang.el
+++ bartuer-erlang.el
@@ -1,10 +1,12 @@
-(require 'erlang-start)
-(require 'distel)
-(setq erlang-root-dir "/usr/local/otp")
+
 
 (defun bartuer-erlang-load ()
   "for erlang language"
+  (require 'erlang-start nil t)
+  (require 'distel nil t)
   (distel-setup)
+  (setq erlang-root-dir "/usr/local/otp")
+  (setq erlang-skel nil)
   )
 
 (provide 'bartuer-erlang)
