6499563f7fa62112101b256b4d22bf2eab7d08ea
bartuer <bartuer@gmail.com>
2011-01-10 21:38:34 +0800

    add distel

--- bartuer-erlang.el
+++ bartuer-erlang.el
@@ -1,8 +1,10 @@
 (require 'erlang-start)
+(require 'distel)
 (setq erlang-root-dir "/usr/local/otp")
 
 (defun bartuer-erlang-load ()
   "for erlang language"
+  (distel-setup)
   )
 
 (provide 'bartuer-erlang)
