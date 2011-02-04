271ea8ba168f4ac7d726ad82845aa872825c01b1
bartuer <bartuer@gmail.com>
2011-02-04 08:18:34 +0800

    start distel_ie as default

--- bartuer-erlang.el
+++ bartuer-erlang.el
@@ -11,7 +11,7 @@
   (if (search-forward-regexp "%#=>" end t)
       t
     nil)
-)
+  )
 
 (defun ect-dwim ()
   "if there is %# =>, remove it, otherwise insert it
@@ -40,6 +40,91 @@ docstring as fixture, try above line.
       (ect-dwim)
     ad-do-it))
 
+(defun otp-grep (command-args)
+  "Run grep over otp documents"
+  (interactive
+   (progn
+     (grep-compute-defaults)
+     (list (read-shell-command "grep otp docs: "
+                               "find ~/local/share/doc/erlang/OTP13B04 -type f  |grep -vE \"BROWSE|TAGS|.svn|drw|Binary|.bzr|svn-base|*.pyc\" |xargs grep -niHE " 'grep-find-history))))
+  (when command-args              
+    (let ((null-device nil))		; see grep
+      (grep command-args))))
+
+(defun erl-choose-emacs-node ()
+  "set default nodename emacs@bartuer ."
+  (interactive)
+  (let* ((nodename-string (if erl-nodename-cache
+			      (symbol-name erl-nodename-cache)
+			    nil))
+	 (name-string "emacs@bartuer")
+         (name (intern (if (string-match "@" name-string)
+                           name-string
+			 (concat name-string
+				 "@" (erl-determine-hostname))))))
+    (setq erl-nodename-cache name)
+    (setq distel-modeline-node name-string)
+    (force-mode-line-update))
+  erl-nodename-cache)
+
+(defun inferior-erlang-without-switch-buffer ()
+  "Run an inferior Erlang.
+
+This is just like running Erlang in a normal shell, except that
+an Emacs buffer is used for input and output.
+\\<comint-mode-map>
+The command line history can be accessed with  \\[comint-previous-input]  and  \\[comint-next-input].
+The history is saved between sessions.
+
+Entry to this mode calls the functions in the variables
+`comint-mode-hook' and `erlang-shell-mode-hook' with no arguments.
+
+The following commands imitate the usual Unix interrupt and
+editing control characters:
+\\{erlang-shell-mode-map}"
+  (interactive)
+  (require 'comint)
+  (let ((opts inferior-erlang-machine-options))
+    (cond ((eq inferior-erlang-shell-type 'oldshell)
+	   (setq opts (cons "-oldshell" opts)))
+	  ((eq inferior-erlang-shell-type 'newshell)
+	   (setq opts (append '("-newshell" "-env" "TERM" "vt100") opts))))
+    (setq inferior-erlang-buffer
+	  (apply 'make-comint
+		 inferior-erlang-process-name inferior-erlang-machine
+		 nil opts)))
+  (setq inferior-erlang-process
+	(get-buffer-process inferior-erlang-buffer))
+  (if (> 21 erlang-emacs-major-version)	; funcalls to avoid compiler warnings
+      (funcall (symbol-function 'set-process-query-on-exit-flag) 
+	       inferior-erlang-process nil)
+    (funcall (symbol-function 'process-kill-without-query) inferior-erlang-process))
+  (with-current-buffer inferior-erlang-buffer
+
+    (if (and (not (eq system-type 'windows-nt))
+             (eq inferior-erlang-shell-type 'newshell))
+        (setq comint-process-echoes t))
+    ;; `rename-buffer' takes only one argument in Emacs 18.
+    (condition-case nil
+        (rename-buffer inferior-erlang-buffer-name t)
+      (error (rename-buffer inferior-erlang-buffer-name)))
+    (erlang-shell-mode))
+  )
+
+(defun start-default-emacs-node ()
+  "start node for distel"
+  (interactive)
+  (if (null (inferior-erlang-running-p))
+      (progn
+        (inferior-erlang-without-switch-buffer) 
+        (sleep-for 0.5)
+        (erl-choose-emacs-node)
+        (erl-ping erl-nodename-cache)
+        )
+    (erl-ping erl-nodename-cache))  
+  )
+
+
 (defun bartuer-erlang-load ()
   "for erlang language"
   (setq erlang-root-dir "/usr/local/otp")
@@ -54,7 +139,13 @@ docstring as fixture, try above line.
   (define-key erlang-mode-map "\M--" (lambda ()
                                        (interactive)
                                        (insert " -> ")) )
+  (save-excursion
+    (start-default-emacs-node)
+    )
   (flymake-mode t)
+  (sleep-for 0.5)
+  (erl-ie-ensure-registered erl-nodename-cache)
+  (erl-session-minor-mode t)
   )
 
 
