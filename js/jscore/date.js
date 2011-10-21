3d141e4b5f2987fe93fe43cb352b360b0eb9d7f3
bartuer <bartuer@gmail.com>
2011-10-21 09:48:11 +0800

    js data comment

--- js/jscore/date.js
+++ js/jscore/date.js
@@ -1,2 +1,18 @@
-var date = {};
+/*
+  Date implement has serious bug
+ */
+
+var t =  new Date();
+t;                               //#=> Fri Oct 21 2011 09:46:51 GMT+0800 (CST)
+t.getDate();                     //#=> 21
+t.getTime();                     //#=> 1319161611839
+t.getMonth();                    //#=> 9
+t.getYear();                     //#=> 111
+
+var f = new Date(2011, 10, 1);
+f.getYear();                    //#=> 111
+f.getMonth();                   //#=> 10
+f.getDate();                    //#=> 1
+
+
 
