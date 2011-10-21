aabce4a2778caf01f5ce3e0ed224ee291ecdcbd6
bartuer <bartuer@gmail.com>
2011-10-21 10:00:48 +0800

    update date function in js core

--- js/jscore/date.js
+++ js/jscore/date.js
@@ -1,18 +1,66 @@
-/*
-  Date implement has serious bug
+var date = {};
+
+/**
+ * Create date
+ * @param none
+ * @return number
+ *
  */
+date.new = function () {
+
+};
+var f = new Date(2011, 10, 1);
 
 var t =  new Date();
-t;                               //#=> Fri Oct 21 2011 09:46:51 GMT+0800 (CST)
+
+/**
+ * Return date number of Date
+ * @param none
+ * @return number
+ *
+ */
+date.getDate = function () {
+
+};
 t.getDate();                     //#=> 21
+
+/**
+ * Return seconds number
+ * @param none
+ * @return number
+ *
+ */
+date.getTime = function () {
+
+};
 t.getTime();                     //#=> 1319161611839
+
+
+/*
+ * getMonth implement has SERIOUS BUG
+ * Return month number
+ * @param none
+ * @return number
+ *
+ */
+date.getMonth = function () {
+
+};
 t.getMonth();                    //#=> 9
+                                 //t #=> Fri Oct 21 2011 09:46:51 GMT+0800 (CST)
+
+/*
+ * Return year number from 1900
+ * @param none
+ * @return number
+ *
+ */
+date.getYear = function () {
+
+};
 t.getYear();                     //#=> 111
 
-var f = new Date(2011, 10, 1);
-f.getYear();                    //#=> 111
-f.getMonth();                   //#=> 10
-f.getDate();                    //#=> 1
+
 
 
 
