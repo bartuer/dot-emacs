9e80ba501a2e47f0065c4f63aa09e235cfe53b46
bartuer <bartuer@gmail.com>
2012-04-24 12:24:07 +0800

    checkjson.js node -> d8 bare

--- vendor/jslint/checkjson.js
+++ vendor/jslint/checkjson.js
@@ -1,13 +1,15 @@
-#!/usr/bin/env node
-
-fs = require('fs');
-fs.readFile(process.argv[2], function (err, data) {
-try {
-  JSON.parse(data);
-  console.log('no problem');
-  process.exit(0);
-} catch(e) {
-  console.log('bad json');
-  process.exit(-1);
-}
-});
+(function (a) {
+  var data = read(a[0]);
+  if (!data) {
+    print("checkjson: Couldn't open file '");
+    return -2;
+  }
+  try {
+    JSON.parse(data);
+    print('no problem');
+    return 0;
+  } catch (e) {
+    print('bad json');
+    return -1;
+  }
+}(arguments));
