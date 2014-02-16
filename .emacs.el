3836471e73efb54cf9db8ca8ba220e870c56f2d6
bartuer <bazhou@microsoft.com>
2014-02-16 16:28:37 +0800

    disable sgml

--- .emacs.el
+++ .emacs.el
@@ -36,6 +36,7 @@
 (global-set-key "\C-ck" 'kill-region) ;delete to the beginning "\C-x-DEL",move 1
 (global-set-key "\C-xk" 'kill-whole-line) ;delete whole line, move 1
 
+(require 'guess-style)
 (autoload 'guess-style-set-variable "guess-style" nil t)
 (autoload 'guess-style-guess-variable "guess-style")
 (autoload 'guess-style-guess-all "guess-style" nil t)
@@ -1074,8 +1075,9 @@ If give a negative ARG, will undo the last mark action, thus the
 (autoload 'bartuer-sql-load "bartuer-sql.el" "for sql mode" t)
 (add-hook 'sql-mode-hook 'bartuer-sql-load)
 
-(autoload 'bartuer-sgml-load "bartuer-sgml.el" "for sgml" t)
-(add-hook 'sgml-mode-hook 'bartuer-sgml-load)
+(add-to-list 'auto-mode-alist '("\\html$" . nxml-mode))
+;; (autoload 'bartuer-sgml-load "bartuer-sgml.el" "for sgml" t)
+;; (add-hook 'sgml-mode-hook 'bartuer-sgml-load)
 
 (autoload 'bartuer-txt-load "bartuer-txt.el" "for text mode" t)
 (add-hook 'text-mode-hook 'bartuer-txt-load)
