e071247eb5f655e6b0839e5cbbe75ec212e51aac
bartuer zhou <bazhou@microsoft.com>
2016-03-03 07:54:20 +0800

    swank server

---
+++ vendor/swank-js/swanknode.el
@@ -0,0 +1,25 @@
+;;; Package --- summary
+;;
+;; A javascript evaluating service
+;;
+;;; Commentary:
+;;
+;; Installation:
+;;
+;; Put this in your load-path, then add the following to your .emacs.
+;; You substitude espresso-mode-hook for javascript-mode-hook if you
+;; use espresso.
+;;
+;;     (require 'swanknode)
+;;
+;; Run M-x swanknode-start once to start the server before invoking flymake.
+
+;;; Code:
+(defun swanknode-start ()
+  "Start the swanknode server."
+  (interactive)
+  (start-process "swanknode-server" "*swanknode*" "~/etc/el/vendor/swank-js/swank.js"
+                 ))
+
+(provide 'swanknode)
+;;; swanknode.el ends here
