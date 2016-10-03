58330890743c67713fc6b232c0c8ebdabce4d04b
bartuer zhou <bazhou@microsoft.com>
2016-10-03 16:48:01 +0800

    fix

--- install/bashrc_linux.sh
+++ install/bashrc_linux.sh
@@ -14,4 +14,4 @@ alias ss='source ~/.bashrc'
 
 export NPM_CONFIG_ELECTRON_MIRROR="https://npm.taobao.org/mirrors/electron/"
 export GZIP_ENV="--rsyncable"
-export PATH=~/local/bin:~/scripts:$PATH
+export PATH=~/.nvm;~/local/bin:~/scripts:$PATH
