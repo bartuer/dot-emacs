0cb926ed7bc96abba9de5b67740216a6d6ad57ff
Bartuer Zhou <bazhou@microsoft.com>
2017-03-06 02:22:47 +0000

    automatically format lua code

--- bartuer-lua.el
+++ bartuer-lua.el
@@ -1,8 +1,10 @@
-(require 'lua nil t)
+(require 'lua-mode nil t)
 
 (defun bartuer-lua-load ()
   "for lua languaage"
   (interactive)
+  (add-hook 'before-save-hook (lambda ()
+                                (indent-region (point-min) (point-max))))
   )
 
 (provide 'bartuer-lua)
