e279697415549cb250ff93a603fdb6204369abd3
bartuer zhou <bazhou@microsoft.com>
2018-04-28 17:43:07 +0800

    24 -> 25

--- install/bashrc_linux.sh
+++ install/bashrc_linux.sh
@@ -15,9 +15,9 @@ function tojson {
 }
 
 alias g='gulp'
-alias e='~/local/bin/emacs-24.5 --daemon -nw -fg yellow'
+alias e='~/local/bin/emacs-25.3 --daemon -nw -fg yellow'
 alias ec='~/local/bin/emacsclient -t'
-alias ed='~/local/bin/emacs-24.5 --debug-init -nw -fg yellow'
+alias ed='~/local/bin/emacs-25.3 --debug-init -nw -fg yellow'
 alias ss='source ~/.bashrc'
 
 export NPM_CONFIG_ELECTRON_MIRROR="https://npm.taobao.org/mirrors/electron/"
