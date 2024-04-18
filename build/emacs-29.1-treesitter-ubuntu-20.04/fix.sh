ab8899790a8dba7f759b7f1631577afceda01e4d
Bartuer <bazhou@microsoft.com>
2024-04-18 12:10:00 +0000

    file list update

--- build/emacs-29.1-treesitter-ubuntu-20.04/fix.sh
+++ build/emacs-29.1-treesitter-ubuntu-20.04/fix.sh
@@ -1,1 +1,1 @@
-docker run -it -v ${PWD}/../el:/root/etc/el -v ${PWD}/:/opt -v ${PWD}/../.ssh:/root/.ssh ubuntu:20.04git /bin/bash
+docker run -it -v ${PWD}/../el:/root/etc/el -v ${PWD}/:/opt -v ${PWD}/../.ssh:/root/.ssh ubuntu:20.04git bash -c "tar zxf /opt/emacs_29.1_20.04.tar.gz -C / && source ~/.bashrc && /bin/bash"
