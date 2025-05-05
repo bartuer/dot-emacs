bf61fa2f8f505f3b03f0782c34688c10f57f4a5a
Bartuer Zhou <bazhou@microsoft.com>
2025-05-05 14:05:49 +0800

    mount home folder

--- build/emacs-30.1-treesitter-ubuntu-22.04/run.sh
+++ build/emacs-30.1-treesitter-ubuntu-22.04/run.sh
@@ -1,1 +1,3 @@
-docker run  --name emacs30.1 -d --hostname jellyfish -p 2222:22 -v ${PWD}/:/opt caapi/arm64.emacs30.1:22.04 
+docker stop -t 0 emacs30.1
+docker rm emacs30.1
+docker run  --name emacs30.1 -d --hostname jellyfish -p 2222:22 -v /Users/bartuer:/opt caapi/arm64.emacs30.1:22.04 
