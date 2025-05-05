7bfa90cd51d860af18ac559ebc8f2162fe893c20
Bartuer Zhou <bazhou@microsoft.com>
2025-05-05 11:13:17 +0800

    arch add to run

--- build/emacs-30.1-treesitter-ubuntu-22.04/run.sh
+++ build/emacs-30.1-treesitter-ubuntu-22.04/run.sh
@@ -1,1 +1,1 @@
-docker run  --name emacs30.1 -it --hostname jellyfish -p 2222:22 -v ${PWD}/:/opt caapi/emacs30.1:22.04  /bin/bash
+docker run  --name emacs30.1 -it --hostname jellyfish -p 2222:22 -v ${PWD}/:/opt caapi/arm64.emacs30.1:22.04  /bin/bash
