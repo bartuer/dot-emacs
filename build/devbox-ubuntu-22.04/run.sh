bde228732b481afa0ff00252e0ddecefc3059cad
Bartuer Zhou <bartuer@gmail.com>
2025-06-13 18:45:02 +0800

    retry rust build

--- build/devbox-ubuntu-22.04/run.sh
+++ build/devbox-ubuntu-22.04/run.sh
@@ -1,3 +1,4 @@
 docker stop -t 0 devbox
 docker rm devbox
-docker run  --name devbox -it  -v /Users/bartuer:/opt ubuntu:22.04
+docker run --name devbox -it  caapi/arm64.devbox:22.04 /bin/bash
+# docker run --name devbox -it  -v /Users/bartuer:/opt ubuntu:22.04
