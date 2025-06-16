295a8ab5bd5e0b198666704c1167c452d403f339
Bartuer Zhou <bartuer@gmail.com>
2025-06-16 16:10:01 +0800

    dev pack

--- build/devbox-ubuntu-22.04/run.sh
+++ build/devbox-ubuntu-22.04/run.sh
@@ -1,4 +1,16 @@
+# docker stop -t 0 devbox
+# docker rm devbox
+# docker run --name devbox -it  caapi/arm64.devbox.build:22.04 /bin/bash
+
+caffeinate docker buildx build  --progress=plain --platform linux/arm64 --build-arg https_proxy=socks5://host.docker.internal:8080 . -t caapi/arm64.devbox.build:22.04
 docker stop -t 0 devbox
 docker rm devbox
-docker run --name devbox -it  caapi/arm64.devbox:22.04 /bin/bash
-# docker run --name devbox -it  -v /Users/bartuer:/opt ubuntu:22.04
+docker run --name devbox -d  caapi/arm64.devbox.build:22.04 /bin/bash
+docker cp devbox:/opt/arm64.dev.js.22.04.tar.gz /Users/bartuer/Downloads
+docker cp devbox:/opt/arm64.dev.rs.22.04.tar.gz /Users/bartuer/Downloads
+docker cp devbox:/opt/arm64.dev.cpp.22.04.tar.gz /Users/bartuer/Downloads
+docker cp devbox:/opt/arm64.dev.py.22.04.tar.gz /Users/bartuer/Downloads
+docker stop -t 0 devbox
+docker rm devbox
+docker run --name devbox -it  -v /Users/bartuer:/opt ubuntu:22.04 /bin/bash
+
