ea24a41a12dd0979645fb1f6568c918f13b362f0
Bartuer Zhou <bartuer@gmail.com>
2025-06-16 19:08:31 +0800

    fix

--- build/devbox-ubuntu-22.04/install.arm64.sh
+++ build/devbox-ubuntu-22.04/install.arm64.sh
@@ -2,6 +2,6 @@ tar zxf arm64.emacs30.1_22.04.tar.gz -C /
 tar zxf arm64.dev.cc.22.04.tar.gz -C /
 tar zxf arm64.dev.py.22.04.tar.gz -C /
 tar zxf arm64.dev.rs.22.04.tar.gz -C /
-tar zxf arm64.dev.rs.22.04.tar.gz -C /
+tar zxf arm64.dev.js.22.04.tar.gz -C /
 tar zxf arm64.dev.base.22.04.tar.gz -C /
 source ~/.bashrc
