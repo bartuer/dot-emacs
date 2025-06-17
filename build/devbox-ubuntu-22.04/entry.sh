0d0d3e93b7740cb1a3cc14ef32573e83a308dfee
Bartuer Zhou <bartuer@gmail.com>
2025-06-17 06:35:49 +0000

    remove sql, lots of dependency

--- build/devbox-ubuntu-22.04/entry.sh
+++ build/devbox-ubuntu-22.04/entry.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ];
 then
     # docker run -d
     service ssh start
-    # nohup bash -c "source /opt/local/src/env.sh; jupyter lab --allow-root --port=8787 --no-browser --notebook-dir=/src " 2> /dev/null &
+    nohup bash -c "source /opt/local/src/env.sh; jupyter lab --allow-root --port=8787 --no-browser --notebook-dir=/opt " 2> /dev/null &
     sleep infinity
 pelse
     # docker run -i CMD
