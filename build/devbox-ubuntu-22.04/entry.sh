72f22e2d0383ed4cb68f998db2ff493552477c46
Bartuer Zhou <bartuer@gmail.com>
2025-06-17 17:43:35 +0800

    jupyter access

--- build/devbox-ubuntu-22.04/entry.sh
+++ build/devbox-ubuntu-22.04/entry.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ];
 then
     # docker run -d
     service ssh start
-    nohup bash -c "jupyter lab --allow-root --port=7878 --no-browser --notebook-dir=/opt " 2> /dev/null &
+    nohup bash -c "source /opt/local/src/env.sh; /root/.local/.venv/bin/jupyter lab --allow-root --ip=0.0.0.0 --port=7878 --no-browser --notebook-dir=/opt " 2> /dev/null &
     sleep infinity
 pelse
     # docker run -i CMD
