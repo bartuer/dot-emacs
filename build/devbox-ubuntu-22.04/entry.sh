f118d291009ba109e0ab3de197d5e38c07bcd487
Bartuer Zhou <bartuer@gmail.com>
2025-06-19 09:54:30 +0000

    remove c++ kernel

--- build/devbox-ubuntu-22.04/entry.sh
+++ build/devbox-ubuntu-22.04/entry.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ];
 then
     # docker run -d
     service ssh start
-    nohup bash -c "source /opt/local/src/env.sh; /root/.local/.venv/bin/jupyter lab --allow-root --ip=0.0.0.0 --port=7878 --no-browser --notebook-dir=/opt " 2> /dev/null &
+    nohup bash -c "source /opt/local/src/env.sh; source /root/.local/.venv/bin/activate; /root/.local/.venv/bin/jupyter lab --allow-root --Application.log_level=DEBUG --ip=0.0.0.0 --port=7878 --no-browser --notebook-dir=/opt " 2> /var/log/jupyter.log &
     sleep infinity
 pelse
     # docker run -i CMD
