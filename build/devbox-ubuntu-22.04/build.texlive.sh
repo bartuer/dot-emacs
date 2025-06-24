fd3e64f75616765056b035be0b8a2bbeb7437a78
Bartuer Zhou <bartuer@gmail.com>
2025-06-24 08:22:05 +0000

    fix bibtex link

--- build/devbox-ubuntu-22.04/build.texlive.sh
+++ build/devbox-ubuntu-22.04/build.texlive.sh
@@ -1,7 +1,8 @@
 caffeinate docker buildx build -f Dockerfile.tex  --progress=plain --platform linux/arm64 --build-arg https_proxy=socks5://host.docker.internal:8080 . -t caapi/arm64.tex.base:22.04
 docker stop -t 0 texbox
 docker rm texbox
-docker run --name texbox -it  caapi/arm64.tex.base:22.04 /bin/bash
+docker run --name texbox -d  caapi/arm64.tex.base:22.04
 docker cp texbox:/opt/arm64.dev.latex.22.04.tar.gz /Users/bartuer/Downloads
+du -sh /Users/bartuer/Downloads/arm64.dev.latex.22.04.tar.gz
 docker stop -t 0 texbox
 docker rm texbox
