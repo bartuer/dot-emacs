7fe4fa8788eefba3eb78eac84120dcf190ae5143
Bartuer Zhou <bartuer@gmail.com>
2025-09-02 06:07:27 +0000

    rebuild emacs package

--- build/emacs-30.1-treesitter-ubuntu-24.04/tar.sh
+++ build/emacs-30.1-treesitter-ubuntu-24.04/tar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd /
-tar czf /opt/amd64.emacs30.1_24.04.tar.gz --exclude="root/etc/el/.git" -T /root/etc/el/build/emacs-30.1-treesitter-ubuntu-24.04/amd64.tar.list
+tar czf /opt/amd64.emacs30.1_24.04.tar.gz --exclude="root/etc/el/.git" -T /opt/amd64.tar.list
 popd
