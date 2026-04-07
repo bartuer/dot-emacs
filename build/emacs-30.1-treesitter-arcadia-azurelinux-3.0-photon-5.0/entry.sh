64603714e1b4ac4fbf763f098744199d2a54845e
Bartuer Zhou <bazhou@microsoft.com>
2026-04-07 09:17:41 +0000

    aspell data and sshd user

--- build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/entry.sh
+++ build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/entry.sh
@@ -3,8 +3,13 @@
 if [ $# -eq 0 ];
 then
     # docker run -d
-    [ -f /etc/ssh/ssh_host_rsa_key ] || ssh-keygen -A
-    /usr/sbin/sshd
+    if command -v sshd >/dev/null 2>&1; then
+        [ -f /etc/ssh/ssh_host_rsa_key ] || ssh-keygen -A
+        # sshd needs a privilege-separation user
+        id sshd >/dev/null 2>&1 || useradd -r -d /var/empty/sshd -s /sbin/nologin sshd 2>/dev/null
+        mkdir -p /var/empty/sshd
+        /usr/sbin/sshd
+    fi
     sleep infinity
 else
     # docker run -i CMD
