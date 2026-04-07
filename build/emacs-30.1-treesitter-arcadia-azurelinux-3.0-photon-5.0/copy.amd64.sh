85158c00a42697fac5a6003b8df363bbdbaea9ac
Bartuer Zhou <bazhou@microsoft.com>
2026-04-07 06:26:55 +0000

    treesitter load

--- build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/copy.amd64.sh
+++ build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/copy.amd64.sh
@@ -3,7 +3,8 @@
 # Run after: tar zxf amd64.arcadia.emacs30.1_azl3.0.tar.gz -C /
 
 # tsc-dyn.so (elisp-tree-sitter Rust binding)
-cp ~/etc/el/build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/amd64/vendor/tsc/tsc-dyn.so ~/etc/el/vendor/tsc
+# The amd64 binary lives under the ubuntu-24.04 build (same glibc ABI).
+cp ~/etc/el/build/emacs-30.1-treesitter-ubuntu-24.04/amd64/vendor/tsc/tsc-dyn.so ~/etc/el/vendor/tsc
 
 # tree-sitter grammar .so files (compiled by treesit-compile.el during docker build)
 # These are already at ~/.emacs.d/tree-sitter/ after tarball extraction.
