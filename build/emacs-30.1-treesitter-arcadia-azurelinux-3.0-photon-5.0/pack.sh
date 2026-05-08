7e036a9539b57f871de073620cbc0c554377f0da
Bartuer Zhou <bazhou@microsoft.com>
2026-05-08 12:19:01 +0000

    vterm land on ubuntu

--- build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/pack.sh
+++ build/emacs-30.1-treesitter-arcadia-azurelinux-3.0-photon-5.0/pack.sh
@@ -7,5 +7,12 @@ set -eu
 
 IMAGE=mcr.microsoft.com/officepy/codeexecutionjupyterext1:emacs30.1.arcadia
 
-docker build --platform linux/amd64 -t "$IMAGE" .
+# DOTEMACS_REV busts the `git clone bartuer/dot-emacs` layer every build
+# so we always ship HEAD (picks up changes to bartuer-vterm.el, .emacs.el,
+# etc.).  Everything above that layer — libvterm, tree-sitter, emacs —
+# is still cached by contents, so this is cheap.
+DOTEMACS_REV=$(date +%s)
+docker build --platform linux/amd64 \
+             --build-arg "DOTEMACS_REV=${DOTEMACS_REV}" \
+             -t "$IMAGE" .
 docker run --rm --platform linux/amd64 --entrypoint sh -w / -v "${PWD}/:/opt" "$IMAGE" /opt/tar.sh
